f0e8b6412b94a021ef75600627b811f70291a505
Gniriki <gniriki@gmail.com>
2019-12-28 13:12:09 +0100

    Added browsers

--- dev_app_unity_gamedev.ps1
+++ dev_app_unity_gamedev.ps1
@@ -26,6 +26,7 @@ executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
+executeScript "Browsers.ps1";
 
 #--- Tools ---
 #--- Installing VS and VS Code with Git
