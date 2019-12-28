6254f6190a3e4b22a50a75be0fc4ad23ba0bd3b6
Gniriki <gniriki@gmail.com>
2019-12-28 13:08:02 +0100

    Added Unity Game Dev setup

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -2,6 +2,11 @@
 # tools we expect devs across many scenarios will want
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y tortoisegit
+choco install -y poshgit
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
+choco install -y greenshot
+choco install -y paint.net
+
